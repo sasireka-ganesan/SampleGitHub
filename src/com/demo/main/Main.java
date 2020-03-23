e674850b2ab3306c63b557aa7b2b7298824419ef
91737 <91737@LAPTOP-SKPG7I8D>
2020-03-23 13:37:21 +0530

    First Commit

---
+++ src/com/demo/main/Main.java
@@ -0,0 +1,12 @@
+package com.demo.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to GitHub");
+
+	}
+
+}
